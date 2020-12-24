8850f77286f9d2f6feb4cb1b86ec986e2f263b1f
Herminio Foloni-Neto <hcfherminio@gmail.com>
2020-12-24 14:12:12 +0900

    Fixing the final output

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,19 @@
-#Final project Getting and Cleanin Data course
+#Final project Getting and Cleaning Data course
 
 library(dplyr)#we are going to use it
 
 #reading files that I will use
-train<-read.table("train/X_train.txt", sep="",header=FALSE)#dataset
-test<-read.table("test/X_test.txt",sep="",header=FALSE)#dataset
+train<-read.table("train/X_train.txt", sep="",header=FALSE)#train dataset
+test<-read.table("test/X_test.txt",sep="",header=FALSE)#test dataset
 trainl<-read.table("train/y_train.txt", sep="",header=FALSE)#dataset label
 testl<-read.table("test/y_test.txt",sep="",header=FALSE)#dataset label
-activity<-read.table("activity_labels.txt")[,-1]
+activity<-read.table("activity_labels.txt")[,-1]#activity labels
+test_subject<-read.table("test/subject_test.txt", sep="",header=FALSE)#subjects used
+train_subject<-read.table("train/subject_train.txt", sep="",header=FALSE)#subjects used
+
+#binding subjects to the dataset
+train<-cbind(train,train_subject)
+test<-cbind(test,test_subject)
 
 #binding both datasets rows
 dataset<-rbind(train,test)
@@ -16,7 +22,7 @@ labels<-rbind(trainl,testl)
 
 #getting column names properly
 cols<-read.table("features.txt",header=FALSE)[,-1]
-colnames(dataset)<-cols
+colnames(dataset)<-c(cols,"Subject")
 colnames(labels)<-"Labels"
 
 #getting labels names instead of numbers
@@ -26,7 +32,8 @@ for(i in c(1:6)){
 #getting the dataset required by the assigment (only means and standard deviations)
 #finding columns with means
 dataset<-dataset[,sort(c(grep("*mean*",colnames(dataset)),
-                         grep("*std*",colnames(dataset))))]
+                         grep("*std*",colnames(dataset)),
+                         grep("Subject",colnames(dataset))))]
 
 #binding dataset with activity labels
 dataset<-cbind(dataset,labels)
@@ -34,7 +41,7 @@ dataset<-cbind(dataset,labels)
 #getting a second data frame with the average as asked in the assigment
 #using dplyr
 mean_dataset<-dataset %>%
-             group_by(Labels) %>%
+             group_by(Subject,Labels) %>%
              summarise_all(list(~mean(.,na.rm=TRUE)))
 
 #writing the output file as asked
